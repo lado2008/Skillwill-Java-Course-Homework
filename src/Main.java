6b4919d9fe7b2429cbe7b206c2dcdde1b3fe3bff
lado2008 <adamialado133@gmail.com>
2025-03-25 16:16:21 +0400

    Java Homework 7

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        String[] array = {"cat", "dog", "car", "toy", "boy", "girl", "bird", "pig", "array", "loop"};
-
-        for (String i : array) {
-            System.out.println(i);
+        
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Lado");
+        words.add("Boy");
+        words.add("Programming");
+        words.add("Java");
+        words.add("Hello");
+        words.add("World");
+        words.add("Python");
+        words.add("Swift");
+        words.add("JavaScript");
+        words.add("C++");
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println(words.get(i));
         }
 
-        getElementIndex(arr, 11);
+
+        ArrayList<Integer> numbers1 = new ArrayList<Integer>();
+        numbers1.add(2);
+        numbers1.add(3);
+        numbers1.add(5);
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        numbers2.add(7);
+        numbers2.add(1);
+        numbers2.add(0);
+
+        System.out.println(greatestNumberFromList(numbers1, numbers2));
     }
-    public static void getElementIndex(int[] arr, int num) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                System.out.println("Index is: " + i);
-                return;
+    public static int greatestNumberFromList(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        int max;
+        int x = list1.get(0);
+        int y = list2.get(0);
+        for (int i = 1; i < list1.size(); i++) {
+            if(list1.get(i) > x) {
+                x = list1.get(i);
             }
         }
-        System.out.println("I couldn't find it");
+        for (int i = 1; i < list2.size(); i++) {
+            if (list2.get(i) > y) {
+                y = list2.get(i);
+            }
+        }
+        if (x > y) {
+             return x;
+        }
+        else {
+             return y;
+        }
     }
 }
