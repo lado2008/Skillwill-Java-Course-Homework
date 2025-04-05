88774419f52e02c046263202b3bc3ce33dd67641
lado2008 <adamialado133@gmail.com>
2025-04-05 17:31:57 +0400

    Java Homework 10

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,48 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Mersedes", "E63S", 2022, 4);
-        System.out.println(car);
-
-        System.out.println(stringToInt("22"));
+        fileManipulation("My name is Lado Adamia");
     }
-    public static int stringToInt(String number) {
-        return Integer.parseInt(number);
+
+    private static void fileManipulation(String text) {
+        File myObj = new File("C:\\Lado Projects\\Java_Projects\\Test_Project\\src\\Text.txt");
+
+        //Create File
+        try {
+            if(myObj.createNewFile()) {
+                System.out.println("File Successfully created!");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (Exception e) {
+            System.out.println("Something Happened! :)");
+        }
+        //Write text in the file
+        try {
+            FileWriter myWriter = new FileWriter(myObj);
+            myWriter.write(text);
+            myWriter.close();
+            System.out.println("Text Successfully added to file");
+        } catch (IOException e) {
+            System.out.println("Something happened");
+            e.printStackTrace();
+        }
+        //Read text form file
+        try {
+            Scanner scanner = new Scanner(myObj);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+            System.out.println("Text Successfully read from file");
+        } catch (FileNotFoundException e) {
+            System.out.println("Something happened");
+        }
     }
 }
